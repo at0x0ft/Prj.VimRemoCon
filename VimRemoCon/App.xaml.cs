672743e373bdab92bcd02c3ff8b869c1c2775f28
at0x0ft <at0p0ft@outlook.com>
2018-11-08 19:24:58 +0900

    Create tasktray resident app prototype.

--- VimRemoCon/App.xaml.cs
+++ VimRemoCon/App.xaml.cs
@@ -13,5 +13,30 @@ namespace VimRemoCon
 	/// </summary>
 	public partial class App : Application
 	{
+		/// <summary>
+		/// Icon in task tray
+		/// </summary>
+		private NotifyIconWrapper _notifyIcon;
+
+		/// <summary>
+		/// Ignite System.Windows.Application.Startup event.
+		/// </summary>
+		/// <param name="e"></param>
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+			this.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+			this._notifyIcon = new NotifyIconWrapper();
+		}
+
+		/// <summary>
+		/// Ignite System.Window.Application.Exit event.
+		/// </summary>
+		/// <param name="e"></param>
+		protected override void OnExit(ExitEventArgs e)
+		{
+			base.OnExit(e);
+			this._notifyIcon.Dispose();
+		}
 	}
 }
---
+++ VimRemoCon/NotifyIconWrapper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VimRemoCon
+{
+	/// <summary>
+	/// Notify icon in task tray
+	/// </summary>
+	public partial class NotifyIconWrapper : Component
+	{
+		/// <summary>
+		/// Initialize NotifyIconWrapper class.
+		/// </summary>
+		public NotifyIconWrapper()
+		{
+			InitializeComponent();
+
+			// Subscribe context menu event.
+			this.toolStripMenuItem_Open.Click += this.ToolStripMenuItem_Open_Click;
+			this.toolStripMenuItem_Exit.Click += this.ToolStripMenuItem_Exit_Click;
+		}
+
+		/// <summary>
+		/// Initialize NotifyIconWrapper class with assigning container.
+		/// </summary>
+		/// <param name="container"></param>
+		public NotifyIconWrapper(IContainer container)
+		{
+			container.Add(this);
+
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Called in selected context menu:"Open".
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void ToolStripMenuItem_Open_Click(object sender, EventArgs e)
+		{
+			// Generate and show MainWindow
+			var wnd = new MainWindow();
+			wnd.Show();
+		}
+
+		/// <summary>
+		/// Called in selected context menu:"Exit".
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void ToolStripMenuItem_Exit_Click(object sender, EventArgs e)
+		{
+			// Shutdown this app.
+			Application.Current.Shutdown();
+		}
+	}
+}
